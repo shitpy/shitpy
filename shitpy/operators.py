345cc3d23dc0dfeb78519979544e73a9374518fc
Jordi Castelló <jordic90@gmail.com>
2017-10-11 01:07:16 +0200

    Start project

---
+++ shitpy/operators.py
@@ -0,0 +1,12 @@
+class Operators:
+    @staticmethod
+    def add(a, b):
+        """ You want to add two numbers. 
+            Why not to concatenate them? :) 
+        """
+        print("Pooping CPU... ", end='')
+        a = str(a)
+        b = str(b)        
+        c = a + b
+        print("Done")
+        return c
