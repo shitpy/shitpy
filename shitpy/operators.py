866bb0c98bee7e392126d987053b9d068fb44314
tanayag <tanay_agrawal@hotmail.com>
2017-10-11 21:48:23 +0530

    some changes

--- shitpy/operators.py
+++ shitpy/operators.py
@@ -1,3 +1,6 @@
+from random import randint
+import time
+
 class int:
     def __init__(self, n):
         self.number = n
@@ -6,11 +9,15 @@ class int:
         """ You want to add two numbers. 
             Why not to concatenate them? :) 
         """
-        print("Pooping CPU... ", end='')
+
+        print("Pooping CPU... ")
+        print("Pooping takes time, I am so Shitty...")
+        t = randint(2, 6)
+        time.sleep(t)
         a = str(self.number)
-        b = str(n)        
+        b = str(n.number)        
         c = a + b
-        print("Done")
+        print("Aaahhhh!! Feels so Good!")
         return c
 
     def __str__(self):
