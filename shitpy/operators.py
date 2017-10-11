776389771d165856ed980bc6e92d71989132f748
Jordi Castelló <jordic90@gmail.com>
2017-10-11 16:18:33 +0200

    Update int class

--- shitpy/operators.py
+++ shitpy/operators.py
@@ -1,12 +1,17 @@
-class Operators:
-    @staticmethod
-    def add(a, b):
+class int:
+    def __init__(self, n):
+        self.number = n
+
+    def __add__(self, n):
         """ You want to add two numbers. 
             Why not to concatenate them? :) 
         """
         print("Pooping CPU... ", end='')
-        a = str(a)
-        b = str(b)        
+        a = str(self.number)
+        b = str(n)        
         c = a + b
         print("Done")
         return c
+
+    def __str__(self):
+        return str(self.number)
