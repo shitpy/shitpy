493e5d7b984df4b4d725ac4a2713450b68f3e5b0
ayooshkathuria <kathuria.ayoosh@gmail.com>
2017-10-22 16:56:53 +0530

    Update __init__.py
    Corrected an error message.

--- shitpy/__init__.py
+++ shitpy/__init__.py
@@ -3,6 +3,6 @@ from .dataStructures import *
 
 import sys
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
-    raise ImportError("Python Version 2.6 or above is required for SymPy.")
+    raise ImportError("Python Version 2.6 or above is required for ShitPy.")
 
 del sys
