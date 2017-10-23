678f2b249953b896a643dabfbee19ca0520c2969
Matilda <jippie95@hotmail.com>
2017-10-23 21:11:45 +0200

    Added make_turd(): automatically creates shit piles.

--- shitpy/dataTypes/int.py
+++ shitpy/dataTypes/int.py
@@ -6,6 +6,18 @@ class int:
     def __init__(self, n):
         self.number = n
 
+    def make_turd(self, levels):
+        '''
+            Creates a pile of shit levels high.
+        '''
+
+        turd = levels * ' ' + '^'
+
+        for i in range(2, levels + 1):
+            turd += ('\n' + (levels - i)* ' ' + '(' + (2*i - 1) *'_' + ')')
+
+        return turd
+
     def __add__(self, n):
         """ You want to add two numbers. 
             Why not to concatenate them? :p
@@ -16,21 +28,14 @@ class int:
         
         if t == 1:
             time.sleep(1)
-            print("""
-                      ^
-                    (___)
-                   (_____)
-                  (_______)
+            print(self.make_turd(4) + 
 
-            Aahhh! AAAHHH! Its huge!""")
+            """Aahhh! AAAHHH! Its huge!""")
             time.sleep(2)
             print("FEELS SO GOOD!")
         else:
             for i in range(t):
-                print("""
-                          ^
-                        (___)
-                       (_____)""")
+                print(self.make_turd(3))
                 time.sleep(1)
             print("Aaahhhh!! Feels so Good!")
         
@@ -42,7 +47,6 @@ class int:
 
     def __mul__(self, n):
         """ You want to multiply two numbers.
-            Why not to concatenate them? :p
         """
         print("Wait! I am Pooping...")
 
@@ -50,12 +54,8 @@ class int:
 
         if t == 1:
             time.sleep(1)
-            print("""
-                      ^
-                    (___)
-                   (_____)
-                  (_______)
-                 (_________)
+            print(self.make_turd(5) + 
+                """
 
             Aahhh! AAAHHH! Its huge!
                  Its bigger!
@@ -65,10 +65,7 @@ class int:
             print("FEELS SO GOOD!")
         else:
             for i in range(t):
-                print("""
-                          ^
-                        (___)
-                       (_____)""")
+                print(self.make_turd(3))
                 time.sleep(1)
             print("Aaahhhh!! Feels so Good!")
 
