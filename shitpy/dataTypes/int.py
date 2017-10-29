e50b6eff4e158fad5b13fc6e1e1c0f445377f066
Zhanet <ivanova.zhanet@gmail.com>
2017-10-29 01:25:24 +0100

    Subtraction shit code added

--- shitpy/dataTypes/int.py
+++ shitpy/dataTypes/int.py
@@ -7,13 +7,13 @@ class int:
         self.number = n
 
     def __add__(self, n):
-        """ You want to add two numbers. 
+        """ You want to add two numbers.
             Why not to concatenate them? :p
         """
         print("Wait! I am Pooping...")
-        
+
         t = randint(1, 6)
-        
+
         if t == 1:
             time.sleep(1)
             print("""
@@ -33,11 +33,11 @@ class int:
                        (_____)""")
                 time.sleep(1)
             print("Aaahhhh!! Feels so Good!")
-        
+
         a = str(self.number)
-        b = str(n.number)        
+        b = str(n.number)
         c = a + b
-        
+
         return int(c)
 
     def __mul__(self, n):
@@ -78,11 +78,58 @@ class int:
 
         return int(c)
 
+    def __sub__(self, n):
+
+        """ You want to subtract two numbers.
+            Why not do this digit by digit and concat? :p
+        """
+        print("Wait! I am Pooping...")
+
+        t = randint(1, 6)
+
+        if t == 1:
+            time.sleep(1)
+            print("""
+                      ^
+                    (___)
+                   (_____)
+                  (_______)
+                 (_________)
+
+            Aahhh! AAAHHH! Its huge!
+                 Its bigger!
+                 Its badder!
+            Its the biggest shit ever""")
+            time.sleep(2)
+            print("FEELS SO GOOD!")
+        else:
+            for i in range(t):
+                print("""
+                          ^
+                        (___)
+                       (_____)""")
+                time.sleep(1)
+            print("Aaahhhh!! Feels so Good!")
+
+        a = [float(num) for num in list(str(self.number)) if num is not "-"]
+        b = [float(num) for num in list(str(n.number)) if num is not "-"]
+        if len(a) > len(b):
+            while(len(a) > len(b)):
+                b.append(0)
+        elif len(b) > len(a):
+            while(len(b) > len(a)):
+                a.append(0)
+        c = ""
+        for n in range(len(a)):
+            c = c + str(a[n] - b[n])
+
+        return c + " is the One trUe shiT Answer"
+
     def __rmul__(self, n):
         return self.__mul__(n)
 
     def __repr__(self):
         return str(self.number)
-        
+
     def __str__(self):
         return str(self.number)
