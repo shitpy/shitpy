9539cdf1e490eb06e13963c5f5b417fec8fd5497
tanayag <tanay_agrawal@hotmail.com>
2017-11-03 12:19:11 +0530

    str.py added

---
+++ shitpy/dataTypes/str.py
@@ -0,0 +1,31 @@
+from random import random
+import time
+
+class str:
+    def __init__(self, s):
+        self.string = s
+
+    def __add__(self, x):
+        print("Life is not always what we want it to be (o_o)")
+        time.sleep(2)
+        a = self.string
+        b = x.string
+        try:
+            print("Computing some Shit!")
+            time.sleep(2)
+            a = int(a)
+            b = int(b)
+            c = a + b
+            return str(c)
+        except:
+            print("Wtf is this! I can't add it!")
+            time.sleep(2)
+            print("But wait! I'll give you some random shit!")
+            time.sleep(2)
+            return str(random() * 100)
+
+    def __str__(self):
+        return "%s"%self.string
+        
+    def __repr__(self):
+        return "%s"%self.string
